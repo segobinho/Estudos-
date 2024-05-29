af34372e67f58aeca19fe3c1b5c445c783b5690d
segobinho <gabrielsegobi@edu.unifil.br>
2024-05-29 17:54:45 -0300

    1

---
+++ estudorc/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+This is a one-time verification of your recent configured 2FA credentials.
+Make sure that 2FA is correctly configured, and avoid a potential account lockout disaster. If you're having trouble verifying, you'll be able to reconfigure 2FA for your account.
+
+
+    </div>
+  );
+}
+
+export default App;
